db885a839226390e4838b340a49c4384df2d9572
green-alien <gkeko@protonmail.com>
2022-06-23 17:14:50 -0700

    doc: give credit

--- src/uci/mod.rs
+++ src/uci/mod.rs
@@ -1,2 +1,8 @@
+/**
+    A special thank you to Dr. Marco Block-Berlitz,
+    for hosting UCI (=universal chess interface). (which I absolultly copied into my project)
+    Vielen Dank.
+    http://page.mi.fu-berlin.de/block/uci.htm
+ */
 pub mod receiver;
 pub mod dispatcher;
